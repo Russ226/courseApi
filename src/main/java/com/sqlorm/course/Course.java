54881e0ee57b55b9a9f032b41dea24ddb8ece7a4
russ226 <russ2510@gmail.com>
2018-05-18 02:07:17 -0400

    Tested connection to database

---
+++ src/main/java/com/sqlorm/course/Course.java
@@ -0,0 +1,119 @@
+package com.sqlorm.course;
+
+import com.sun.istack.internal.NotNull;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name = "name")
+    @NotNull
+    private String name;
+
+    @Column(name = "code")
+    @NotNull
+    private int code;
+
+    @Column(name = "semester")
+    @NotNull
+    private String semester;
+
+    @Column(name = "year")
+    @NotNull
+    private int year;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST,CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(name = "courseJoinStudent",
+            joinColumns=@JoinColumn(name="course_id"),
+            inverseJoinColumns=@JoinColumn(name="student_id"))
+    private List<Student> students;
+
+    Course(){
+
+    }
+
+    public Course(String name, int code, String semester, int year) {
+        this.name = name;
+        this.code = code;
+        this.semester = semester;
+        this.year = year;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", code=" + code +
+                ", semester='" + semester + '\'' +
+                ", year=" + year +
+                ", students=" + students +
+                '}';
+    }
+}
---
+++ src/main/java/com/sqlorm/course/Student.java
@@ -0,0 +1,103 @@
+package com.sqlorm.course;
+
+import com.sun.istack.internal.NotNull;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="firstName")
+    @NotNull
+    private String firstName;
+
+    @Column(name="lastName")
+    @NotNull
+    private String lastName;
+
+    @Column(name="credits")
+    private Integer credits;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST,CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(name = "courseJoinStudent",
+            joinColumns=@JoinColumn(name="student_id"),
+            inverseJoinColumns=@JoinColumn(name="course_id"))
+    private List<Course> courses;
+
+    public Student(){
+
+    }
+
+    public Student(String firstName, String lastName, int credits) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.credits = credits;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", credits=" + credits +
+                ", courses=" + courses +
+                '}';
+    }
+}
